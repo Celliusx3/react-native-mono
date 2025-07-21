816fa20dce02811a5620fb4802339f567c6761b1
Cellius <cellstudiox3@gmail.com>
2025-07-22 07:57:17 +0800

    chore: [RNMONO-0] Remove unused components

--- apps/podcast-app/app/podcast/_layout.tsx
+++ apps/podcast-app/app/podcast/_layout.tsx
@@ -5,7 +5,6 @@ export default function PodcastLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ header: () => <AppBar title="Podcast" /> }} />
-      <Stack.Screen name="player-screen" options={{ header: () => <AppBar title="Player" /> }} />
       <Stack.Screen name="podcast-screen" options={{ header: () => <AppBar title="" /> }} />
     </Stack>
   );
